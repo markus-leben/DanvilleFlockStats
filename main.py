3544cac3daac9abed0e6aa26dd1b488424b1f8f5
Markus Leben <markus_leben@mac.com>
2026-01-02 18:20:49 -0700

    added rounding, updated generic terms

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ reasons = {}
 for chunk in chunk_iterator:
   for index, row in chunk.iterrows():
     for suffix in ['', '.1', '.2', '.3', '.4', '.5']:
-      if not isActuallyNan(row['Name'+suffix]):
+      name = row['Name'+suffix]
+      if not isActuallyNan(name):
         count += 1
         reason = str(row['Reason'+suffix]).lower().strip()
         if reason == 'nan':
@@ -49,13 +50,13 @@ for chunk in chunk_iterator:
 reasons_by_frequency = dict(sorted(reasons.items(), key= lambda tup: tup[1]))
 bs_reason_count = 0
 for key, value in reasons_by_frequency.items():
-   if len(key) < 2 or key in ['info', 'information', 'inv', 'investigation', 'suspect', '[blank or whitespace]', 'suspicious', 'investigations', 'tbd', 'search', 'query', 'investigate', 'training', 'research', 'invst', 'investigaton', '---', 'suspicious activity', 'location', ]:
+   if len(key) < 2 or key in ['info', 'information', 'inv', 'investigation', 'suspect', '[blank or whitespace]', 'suspicious', 'investigations', 'tbd', 'search', 'query', 'investigate', 'training', 'research', 'invst', 'investigaton', '---', 'suspicious activity', 'location', 'suspicous', 'sus', 'sus auto', 'suspicious vehicle', 'testing', 'test', 'inestigation', 'all', 'locations', 'nunya', 'police']:
      bs_reason_count += value
    if value > 100:
     print(f'{key}: {value}')
 
 print('------')
-print(f'in state: {instate_count}, {100*instate_count/count}% of total')
-print(f'with a case: {has_case_count}, {100*has_case_count/count}% of total')
-print(f'with a short, generic, or blank reason: {bs_reason_count}, {100*bs_reason_count/count}% of total')
+print(f'in state: {instate_count}, {round(100*instate_count/count, 2)}% of total')
+print(f'with a case: {has_case_count}, {round(100*has_case_count/count, 2)}% of total')
+print(f'with a short, generic, or blank reason: {bs_reason_count}, {round(100*bs_reason_count/count, 2)}% of total')
 print(f'total: {count}')
